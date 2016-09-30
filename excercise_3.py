3ae7e91e7c359cb5598d7e9dc680d7987abefc96
drr117 <ibexradius@gmail.com>
2016-09-30 02:21:46 -0400

    Added many more lines; made variables more specific and included conversion detaills.

--- excercise_3.py
+++ excercise_3.py
@@ -1,42 +1,44 @@
-a = input("SELECT USC OR METRIC AS YOUR UNIT CHOICE; (SELECT 'METRIC' or 'USC')") #Create an input statement to let the user select what they want
-b = float(input("How far did you travel?")) #create an input statement for distance
-c = float(input("How Much gas did you use?")) #do the same for gas usage
-d = b/c #calculate fuel usage
-if (a == "USC"):
-    print (b, "miles")
-    print (c, "gallons")
-    print (d, "mpg")
-    e = d * (62.1371/26.4172)
-    print (e)
-    if (e > 20):
-        print("Extremely Poor")
-    elif (e > 15 and e <= 20):
-        print("Poor")
-    elif (e > 10 and e <= 15):
-        print ("Average")
-    elif (e > 8 and e <= 10):
-        print ("Good")
-    elif (e <= 8):
-        print ("Excellent")
+units = input("SELECT USC OR METRIC AS YOUR UNIT CHOICE; (SELECT 'METRIC' or 'USC')") #Create an input statement to let the user select what they want
+distance = float(input("How far did you travel?")) #create an input statement for distance
+fuel = float(input("How Much gas did you use?")) #do the same for gas usage
+d = distance/fuel #calculate fuel usage
+if (units == "USC"):
+    the_distance = (distance, "miles")
+    the_fuel = (fuel, "gallons")
+    the_fuel_consumption = (d, "mpg")
+    the_distance_convert = (the_distance * 1.60934, "km")
+    the_fuel_convert = (the_fuel * 3.78541, "Liters")
+    rating = the_fuel_consumption * (62.1371/.264172)
+    the_fuel_consumption_convert = (the_fuel_consumption * (62.1371/.264172), "Liters/1OOkm")
+    if (rating > 20):
+        fuel_rating = ("Extremely Poor")
+    elif (rating > 15 and rating <= 20):
+        fuel_rating = ("Poor")
+    elif (rating > 10 and rating <= 15):
+        fuel_rating = ("Average")
+    elif (rating > 8 and rating <= 10):
+        fuel_rating =  ("Good")
+    elif (rating <= 8):
+        fuel_rating = ("Excellent")
 else:
-    if (a == "METRIC"):
-        print (b, "Kilometers")
-        print (c, "Liters")
-        f = d *(1/100)
-        print (d, "Liters/100km")
-        e = f
-        print (e)
-        if (e > 20):
-            print("Extremely Poor")
-        elif (e > 15 and e <= 20):
-            print("Poor")
-        elif (e > 10 and e <= 15):
-            print ("Average")
-        elif (e > 8 and e <= 10):
-            print ("Good")
-        elif (e <= 8):
-            msg_alpha = ("Excellent")
-
+    if (units == "METRIC"):
+        the_distance = (distance, "kilometers")
+        the_fuel = (fuel, "Liters")
+        the_fuel_consumption = (d * 100, "Liters/100Km")
+        the_distance_convert = (the_distance / 1.60934, "Miles")
+        the_fuel_convert = (the_fuel * 3.78541, "Gallons")
+        rating = the_fuel_consumption
+        the_fuel_consumption_convert = (the_fuel_consumption * (62.1371 / .264172), "mpg")
+        if (rating > 20):
+            fuel_rating = ("Extremely Poor")
+        elif (rating > 15 and rating <= 20):
+            fuel_rating = ("Poor")
+        elif (rating > 10 and rating <= 15):
+            fuel_rating = ("Average")
+        elif (rating > 8 and rating <= 10):
+            fuel_rating = ("Good")
+        elif (rating <= 8):
+            fuel_rating = ("Excellent")
 
 
 
